e149a0103f174724ddf59fbce0c90ee78c69c333
Matteo <matteop18@gmail.com>
2020-02-06 12:23:53 +0100

    prendere il valore di input premendo sul bottone

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-alert('ciao');
+// Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il  bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
+// Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
+// Titolo
+// Titolo Originale
+// Lingua
+// Voto
+
+$(document).ready(function() {
+  // Prendo il valore di input quando clicco sul bottone
+  $('#input').on('click', function() {
+    var element = $(this);
+    console.log(element);
+    $('#button').on('click', function() {
+      var valInput = element.val();
+      console.log(valInput);
+    })
+  })
+
+
+});
