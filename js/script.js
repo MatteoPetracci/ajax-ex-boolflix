06f43da642e7420bff2a9f495b58eee1a1c92ccc
Matteo <matteop18@gmail.com>
2020-02-07 13:54:16 +0100

    inizio funzione print star

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
+// MILESTONE 1
+
 // Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il  bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
 // Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
 // Titolo
 // Titolo Originale
 // Lingua
 // Voto
+// *******************************
+
+// MILESTONE 2
+
+// Milestone 2:
+// Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da
+// permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5,
+// lasciando le restanti vuote (troviamo le icone in FontAwesome).
+// Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze
+// piene (o mezze vuote :P)
+// Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della
+// nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della
+// nazione ritornata dall’API (le flag non ci sono in FontAwesome).
+// Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca
+// dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando
+// attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di
+// risposta diversi, simili ma non sempre identici)
+// *********************************************************
 
 $(document).ready(function() {
     $('#button').on('click', function() {
@@ -92,4 +112,15 @@ function numIntgr(num){
     return 5;
     // console.log(numIntgr);
   }
+}
+printStar(3)
+// Stampa stelle
+function printStar(num){
+  for (var i = 1; i <= 5; i++) {
+    console.log(i);
+    if (i < num) {
+      console.log('ciao');
+    }
+  }
+
 }
