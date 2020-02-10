628489543c2e410e6d0488739b3c0f853030e390
Matteo <matteop18@gmail.com>
2020-02-10 15:59:14 +0100

    prove con hover

--- js/script.js
+++ js/script.js
@@ -42,6 +42,12 @@ $(document).ready(function() {
       $('input').val('');
     }
   });
+  $('.list_movie').on('hover', function() {
+    $('ul.box_handle').addClass('flex');
+  })
+  $('.list_tv_show').on('hover', function() {
+    $('ul.box_handle').addClass('flex');
+  })
 });
 
 // **********Funzioni**********
